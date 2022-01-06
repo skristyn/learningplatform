bc74b5488e51e8f491a3bfa77b15a762555681c8
mvickers <vickers.mike@gmail.com>
2022-01-05 21:18:34 -0500

    Added a check for authentication on api root url

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest, JsonResponse
+from django.urls import path
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.urls import path
 from wagtail.api.v2.views import BaseAPIViewSet
 from wagtail.api.v2.utils import get_object_detail_url
 from materials.models import Textbook, Section
@@ -26,13 +26,20 @@ class RootViewSet(BaseAPIViewSet):
         : / Doing this so we're not returning a raw JsonResponse, but a drf one.
         Kinda a framework carcrash.
         """
-        # The api_view decorator expects to wrap a simple function-based django views
-        # not a wagtail APIViewset method. So wrapping an inner function. All knees
-        # and elbows.
+        
+        # if the user is not logged in, they'll have all kinds of errors.
+
+        if not request.user.is_authenticated:
+            return Response ({ "message": "Please log in to view any learning platform content"})
+        
         router = self.get_serializer_context()["router"]
         course = request.user.enrollment.active_course
         next_section = course.specific.next_section(request.user)
         print(next_section)
+
+        # The api_view decorator expects to wrap a simple function-based django views
+        # not a wagtail APIViewset method. So wrapping an inner function. All knees
+        # and elbows.
 
         @api_view(["GET"])
         def inner_view(request, *args, **kwargs):
