5e6632b035082996c65a7453eef32f8e59da6d3a
Potato Dax <beatrizx@umich.edu>
2022-09-22 15:31:41 -0500

    moved login form styles

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,28 +1,29 @@
 <template>
-<div class="login-grid">
-  <div class="row">
-    <h1 class="title">Hi! We’re glad you’re here.</h1>
-  </div>
-  <div class="form-row">
-    <div class="loginForm">
-      <h2>Here for class?</h2>
-      <form @submit="logIn">
-        <input type="text" /><br />
-        <input type="password" /><br />
-        <button>Sign In</button>
-      </form>
+  <div class="login-grid">
+    <div class="row">
+      <h1 class="title">Hi! We’re glad you’re here.</h1>
+    </div>
+    <div class="form-row">
+      <div class="loginForm">
+        <h2>Here for class?</h2>
+        <form @submit="logIn">
+          <input type="text" /><br />
+          <input type="password" /><br />
+          <button>Sign In</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="row">
+      <h2>Want to know more?</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sequi
+        natus delectus eligendi similique quia aperiam hic alias id sit tempora
+        molestias rerum dolorem omnis recusandae, vel soluta officia voluptate!
+      </p>
     </div>
   </div>
-
-  <div class="row">
-    <h2>Want to know more?</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sequi natus
-      delectus eligendi similique quia aperiam hic alias id sit tempora molestias
-      rerum dolorem omnis recusandae, vel soluta officia voluptate!
-    </p>
-  </div>
-</div> <!-- end grid div -->
+  <!-- end grid div -->
 </template>
 
 <script lang="ts">
@@ -65,7 +66,10 @@ button {
 
 @media screen and (min-width: 950px) {
   .login-grid {
-    grid-template-columns: minmax(10px, 1fr) minmax(100px, 4fr) minmax(10px, 1fr);
+    grid-template-columns: minmax(10px, 1fr) minmax(100px, 4fr) minmax(
+        10px,
+        1fr
+      );
   }
 }
 
@@ -78,4 +82,27 @@ div.row {
   justify-self: center;
 }
 
+/* Form styling */
+
+input {
+  border: 1.5px solid #979797;
+  border-radius: 3px;
+  -webkit-box-shadow: inset 0px 2px 4px rgba(50, 50, 50, 0.15);
+  -moz-box-shadow: inset 0px 2px 4px rgba(50, 50, 50, 0.15);
+  box-shadow: inset 0px 2px 4px rgba(50, 50, 50, 0.15);
+  padding: 0.7em;
+  font-size: 1em;
+  margin-bottom: 1em;
+  width: 15em;
+}
+
+form button {
+  color: var(--var-color-pink);
+  font-weight: 800;
+  font-size: 1.75em;
+  padding: 10px;
+  border: 3px solid var(--var-color-pink);
+  border-radius: 8px;
+  background-color: #fff;
+}
 </style>
